67455667f9811abbc4c36bc26a0b251bea9af70e
jooyeonthemaster <jooyeonthemaster@gmail.com>
2025-06-25 14:42:14 +0900

    feat: Add Google Tag Manager (GTM) tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" className="h-full">
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WHKCSC8S');`,
+          }}
+        />
+      </head>
       <body className="h-full lab-notebook" suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WHKCSC8S"
+            height="0" 
+            width="0" 
+            style={{display:'none', visibility:'hidden'}}
+          />
+        </noscript>
+        
         <div className="min-h-full bg-lab-paper relative">
           <div className="absolute inset-0 bg-lab-paper/90 pointer-events-none" />
           <div className="relative z-10">
